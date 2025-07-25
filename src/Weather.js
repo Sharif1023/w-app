b673fb6b9ec5a6e5febaaf968882f84725a870d3
Sharif1023 <sharifislam02001@gmail.com>
2025-07-25 21:57:58 +0600

    .

--- src/Weather.js
+++ src/Weather.js
@@ -21,7 +21,7 @@ const Weather = ({ data, lang }) => {
         {t.condition}: {data.weather[0].main}
       </h3>
       <img
-        src={`.png`}
+         src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
         alt="weather-icon"
       />
       <p>🌡 {t.temperature}: {data.main.temp}{t.unitTemp}</p>
